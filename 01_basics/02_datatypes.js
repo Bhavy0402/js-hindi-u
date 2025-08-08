8f7e1f7925aa84bfc1147e75957c2f46d6dad601
Bhavy0402 <bhavysuhagiya16@gmail.com>
2025-08-08 09:46:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* it is use for running a newer version of js code it
+it help us to treat all JS code as newer virson 
+*/
+
+//alert("hello")// we are using node js not browser
+
+console.log(3+2);
+console.log("bhavy");
+
+/* code readability should be high 
+otherwise we can't get good impression from the company
+*/
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
