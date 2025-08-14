277ca8c7c8630660d537313d623f1ffe49fa9618
Bhavy0402 <bhavysuhagiya16@gmail.com>
2025-08-14 14:04:58 +0000

    conversion and operations

---
+++ 01_basics/03_co.js
@@ -0,0 +1,75 @@
+
+// here co means conversion and operation
+
+let score = "0"
+let score1 = Boolean(score)
+
+//console.log( score1);
+
+
+/*whwnever we type the value in this ("") tyoe of symbol so its 
+called string 
+*/
+
+
+
+/*here some notes and example of hitesh choudhary 
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+*/
+
+
+// ************************************************ opreration *******************************************************
+
+// let value = 3
+// let nvalue = -value
+
+// console.log(nvalue);
+
+// let str1 = "bhavy"
+// let str2 = " suhagiya"
+// let str3 = str1+str2
+
+// console.log(str3);
+
+//  let num1, num2, num3
+
+//  num1 = num2 = num3 = 2+2
+
+ let gamecounter = 100
+ gamecounter++;
+// console.log(gamecounter);
+ 
+let a = 5
+let b = ++a;
+
+console.log(a,b);
+
